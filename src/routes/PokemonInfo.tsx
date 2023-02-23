f6081469a25ffedd1e5fae283be4e52ae80727ff
saubuny <122068609+saubuny@users.noreply.github.com>
2023-02-23 16:05:52 -0500

    fetching for individual pokemon pages

--- src/routes/PokemonInfo.tsx
+++ src/routes/PokemonInfo.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react';
+import { MainClient, Pokemon } from 'pokenode-ts';
+import { FC, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 // I dont know why i have to use a type alias here instead of an interface
@@ -9,8 +10,29 @@ type PokemonInfoParams = {
 const PokemonInfo: FC = () => {
 	// Get ID from useParams
 	const { id } = useParams<PokemonInfoParams>();
-	console.log(id);
-	return <p>{id}</p>;
+
+	// Repeated code :(
+	const [poke, setPoke] = useState<Pokemon>();
+
+	useEffect(() => {
+		const pokeByid = async (id: number) => {
+			const api = new MainClient();
+			await api.pokemon
+				.getPokemonById(id)
+				.then((data) => setPoke(data))
+				.catch((err) => {
+					console.log(err);
+				});
+		};
+
+		pokeByid(id as unknown as number);
+	}, []);
+
+	return (
+		<div>
+			<p> {poke?.name}</p>
+		</div>
+	);
 };
 
 export default PokemonInfo;
