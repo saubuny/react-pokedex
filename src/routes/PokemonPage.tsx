e55c36e12b1b547a96ba34d5f05c1956d74ef53b
saubuny <122068609+saubuny@users.noreply.github.com>
2023-02-28 09:23:58 -0500

    Pagination full implemented

--- src/routes/PokemonPage.tsx
+++ src/routes/PokemonPage.tsx
@@ -5,7 +5,7 @@ import { CardType } from "../extra/CardType";
 
 // TODO: move the id list from page math to the cardlist itself, so it'd take pages instead of an idList
 const PER_PAGE = 100;
-const MAX_POKE_ID = 1008;
+const MAX_POKE_ID = 1008; // TODO: Perhaps find a way to get this info from the API ?
 
 const PokemonPage: FC = () => {
   const [page, setPage] = useState<number>(1);
@@ -13,23 +13,17 @@ const PokemonPage: FC = () => {
 
   const changePage = (page: number) => {
     if (page < 1) return;
-    if (page > 10000) return; // TODO: Add real limit for pagination
+    if (page > Math.ceil(MAX_POKE_ID / PER_PAGE)) return; // TODO: Add real limit for pagination
 
     setPage(page);
     const newIds = [];
     for (let i = page * PER_PAGE - (PER_PAGE - 1); i <= page * PER_PAGE; i++)
-      newIds.push(i);
+      if (i <= MAX_POKE_ID) newIds.push(i);
     setIds(newIds);
   };
 
   // We need to be able to see pokemon without having to change the page first
   useEffect(() => changePage(10), []);
-
-  // TODO: PLEASE FIND A WAY TO FIND MAX 
-  useEffect(() => {
-    console.log(page);
-    console.log((ids[ids.length -1 ] % PER_PAGE) );
-  },[page])
 
   return (
     <>
